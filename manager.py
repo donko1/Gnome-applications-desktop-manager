cd7d6cfcd9b40c4effb5ff436c8cb0e8119142eb
donko1 <zalbertic@mail.ru>
2024-02-22 20:29:48 +0300

    Adding deleting and editing method. Minor changes

--- manager.py
+++ manager.py
@@ -13,8 +13,10 @@ class ApplicationManager:
 	def __init__(self):
 		self.folder_path: str = os.path.expanduser('~/.local/share/applications')
 		self.folder_path_global: str = os.path.expanduser('/usr/share/applications')
+		
 		if not self.folder_path or not self.folder_path_global:
 			raise CannotReadApplicationsFolder
+		
 		self.form: str = """[Desktop Entry]
 Version=1.0
 Terminal={3}
@@ -24,14 +26,25 @@ Exec={1}
 Icon={2}
 """
 
+
+	def __format_dict_to_desktop(self, d: dict) -> str:
+		return self.form.format(d["Name"], d["Exec"], d["Icon"], str(d["Terminal"]).lower())
+
+
 	@staticmethod
 	def __format_desktop_to_dict(desktop_content: str) -> dict:
 		out: dict = {}
 		split_text: tuple = tuple(desktop_content.split("\n"))
+
 		for text in split_text:
-			if text[0:4] in ["Name", "Exec", "Icon"]: # if we see Name, Exec or Icon setting
-				out[text[0:4]] = text[5:]
+			if text[0:4] in ["Name", "Exec", "Icon", "Term"]: # if we see Name, Exec or Icon setting
+				if text[0:4] != "Term":
+					out[text[0:4]] = text[5:]
+					continue
+				out[text[0:8]] = True if text[9:] == "true" else False
+
 		return out
+
 
 	@classmethod
 	def __get_all_applications_from_folder(cls, folder: str) -> list:
@@ -39,15 +52,22 @@ Icon={2}
 		files: list = os.listdir(folder)
 		for file in files:
 			path_to_file: str = f"{folder}/{file}" 
+
 			with open(path_to_file, "r") as f:
 				text: str = f.read()
-				out.append(cls.__format_desktop_to_dict(text))
+				info: dict = cls.__format_desktop_to_dict(text)
+				info["file"] = path_to_file
+				out.append(info)
+
 		return out
+
 
 	def get_all_applications(self):
 		local_applications: tuple = tuple(self.__get_all_applications_from_folder(self.folder_path))
 		glob_applications: tuple = tuple(self.__get_all_applications_from_folder(self.folder_path_global))
+
 		return (local_applications, glob_applications)
+
 
 	def create_desktop_file(
 		self,
@@ -63,7 +83,13 @@ Icon={2}
 			raise CannotMoveNotExistingFileError("File is not exists!")
 			return
 
-		code: str = self.form.format(name_of_app, command, icon_path, str(terminal).lower()) # Making code of desktop file
+		code: str = self.__format_dict_to_desktop({
+			"Name": name_of_app, 
+			"Icon":icon_path, 
+			"Terminal": terminal, 
+			"Exec": command}
+		)
+
 		if do_write_in_file:
 			with open(f"{name_of_app}.desktop", "w") as f:
 				f.write(code)
@@ -76,9 +102,26 @@ Icon={2}
 		
 		return code
 
+	def edit_application(self, application_path: str, new_data: dict) -> None:
+		if os.path.isfile(application_path):
+			code = self.__format_dict_to_desktop(new_data) 
+			with open(application_path, "w") as f:
+				f.write(code)
+			return
+
+		raise FileExistsError("Editing file is not exists")
+
+	def delete_application(self, application_path) -> None:
+		if os.path.isfile(application_path):
+			os.remove(application_path)
+			return
+
+		raise FileExistsError("File is not exists now")
+
 def main():
 	manager = ApplicationManager()
-	manager.get_all_applications()[1]
+	# print(manager.get_all_applications())
+	# manager.delete_application("test.desktop")
 	# manager.create_desktop_file("test", "test", "test", True)
 
 if __name__ == '__main__':
