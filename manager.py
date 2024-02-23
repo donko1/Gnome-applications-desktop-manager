3e5c59a4300f7db12fce1aaca5a6d0ca67b83a1e
donko1 <zalbertic@mail.ru>
2024-02-23 19:00:31 +0300

    minor changes

--- manager.py
+++ manager.py
@@ -1,5 +1,5 @@
 import os
-# from pprint import pprint as print # This line is for debug
+from pprint import pprint as print # This line is for debug
 
 class CannotMoveNotExistingFileError(Exception):
 	def __init__(self, *args, **kwargs):
@@ -52,12 +52,12 @@ Icon={2}
 		files: list = os.listdir(folder)
 		for file in files:
 			path_to_file: str = f"{folder}/{file}" 
-
-			with open(path_to_file, "r") as f:
-				text: str = f.read()
-				info: dict = cls.__format_desktop_to_dict(text)
-				info["file"] = path_to_file
-				out.append(info)
+			if path_to_file.endswith(".desktop"):
+				with open(path_to_file, "r") as f:
+					text: str = f.read()
+					info: dict = cls.__format_desktop_to_dict(text)
+					info["file"] = path_to_file
+					out.append(info)
 
 		return out
 
@@ -74,7 +74,7 @@ Icon={2}
 		name_of_app: str,
 		icon_path: str,
 		command: str,
-		terminal: bool,
+		terminal: bool=False,
 		do_write_in_file: bool=True,
 		move_file_to_application: bool=True,
 		localFolder: bool=True
@@ -116,13 +116,16 @@ Icon={2}
 			os.remove(application_path)
 			return
 
+		newPath1 = f"{self.folder_path}/{application_path}"
+		newPath2 = f"{self.folder_path_global}/{application_path}"
+		print(newPath1)
+		if os.path.isfile(newPath2) or os.path.isfile(newPath1):
+			os.remove(newPath1 if os.path.isfile(newPath1) else newPath2)
 		raise FileExistsError("File is not exists now")
 
 def main():
 	manager = ApplicationManager()
-	# print(manager.get_all_applications())
-	# manager.delete_application("test.desktop")
-	# manager.create_desktop_file("test", "test", "test", True)
+	# Ur tests here
 
 if __name__ == '__main__':
 	main()
