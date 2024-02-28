b81a2f88a9a942c3a7e7e13d50e9f583aefb1d60
donko1 <zalbertic@mail.ru>
2024-02-28 17:50:53 +0300

    Upgrading start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
 
 #!/bin/bash
 
-script_dir=$(dirname "$0")
-cd $script_dir
+cd "$(dirname "$0")"
 
+if [ ! -d "venv" ]; then
+    python3 -m venv venv --without-pip
+	sudo apt-get install xclip
+	sudo apt-get install python3-tk
+	sudo apt install git
+
+	PYTHON_VERSION=$(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)')
+	PYTHON_VENV="python${PYTHON_VERSION:0:4}-venv"
+	sudo apt install $PYTHON_VENV
+
+	source venv/bin/activate
+	curl https://bootstrap.pypa.io/get-pip.py | python
+	pip3 install -r requirements.txt
+fi
 source venv/bin/activate
-pip3 install -r requirements.txt
 python3 app.py
