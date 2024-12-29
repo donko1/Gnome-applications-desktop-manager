9c0bd3d719c8b6989fc689de9834a1c91f58c275
donko1 <zalbertic@mail.ru>
2024-12-30 02:23:07 +0300

    Fixing bug with language

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
-
-
 import json
 import os
 
@@ -8,14 +6,21 @@ class Settings:
         if not os.path.isfile('settings.json'):
             with open("settings.json", "w") as file:
                 json.dump({"Language":"English", "ExtendedSettings": False}, file, indent=4)
+            # Set permissions to allow read/write for all users
+            os.chmod("settings.json", 0o666)
 
         with open('settings.json', 'r') as file:
             self.data = json.load(file)
     
     def write_data(self, key, value):
         self.data[key] = value
+        # Ensure file has correct permissions before writing
+        if os.path.exists('settings.json'):
+            os.chmod("settings.json", 0o666)
         with open('settings.json', 'w') as file:
             json.dump(self.data, file, indent=4)
+        # Set permissions after writing as well
+        os.chmod("settings.json", 0o666)
 
     def get_data(self, key):
         with open('settings.json', 'r') as file:
